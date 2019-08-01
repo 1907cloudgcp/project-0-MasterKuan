f21f766537aeaff61c29f905fb6927d35cd0f042
wfong <wfong@eecs.wsu.edu>
2019-08-01 10:44:40 -0400

    Created basic menu with input reading.

---
+++ src/main/python/com/revature/controller/redirector.py
@@ -0,0 +1,55 @@
+def main_menu():
+    while True:
+        print("Actions:\n"
+              "    Register\n"
+              "    Login")
+
+        user_input = input("Input: ").lower()
+        action = parse(0, user_input)
+
+        if action == 0:
+            break
+        elif action == 1:
+            print("Register success")
+        elif action == 2:
+            username = input("Username: ")
+            password = input("Password: ")
+            print("Logging in as " + username)
+
+def account_menu():
+    while True:
+        print("Actions:\n"
+              "    Deposit\n"
+              "    Withdraw\n"
+              "    History\n"
+              "    Logout")
+        user_input = input("Input: ").lower()
+        if not parse(1, user_input):
+            break
+
+def parse(menu, action):
+
+    if (action == "exit"):
+        print("Exiting")
+        return 0
+    elif (action == "register" and menu == 0):
+        print("Registering")
+        return 1
+    elif (action == "login" and menu == 0):
+        print("Logging in")
+        return 2
+    elif (action == "logout" and menu == 1):
+        print("Logging out")
+        return 0
+    elif (action == "deposit" and menu == 1):
+        print("Depositing")
+        return 1
+    elif (action == "withdraw" and menu == 1):
+        print("Withdrawing")
+        return 2
+    elif (action == "history" and menu == 1):
+        print("Viewing transactions history")
+        return 3
+    else:
+        print("Input was not recognized.")
+        return 4
--- src/main/python/com/revature/main.py
+++ src/main/python/com/revature/main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+from controller.redirector import *
 
-'''
-This is your main script, this should call several other scripts within your packages.
-'''
 def main():
-	print('TO-DO')
+	main_menu()
 
 if __name__ == '__main__':
 	main()
